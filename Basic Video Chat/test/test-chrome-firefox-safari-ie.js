b966b253526552828f6ac2911cf4276eb8c5f984
Travis CI <adam@tokbox.com>
2018-02-12 22:01:27 +0000

    Deploy to GitHub Pages: d6bb4ad4cb0d969f872c0996f68906fff12e76c9

--- Basic Video Chat/test/test-chrome-firefox-safari-ie.js
+++ Basic Video Chat/test/test-chrome-firefox-safari-ie.js
@@ -1,6 +1,6 @@
 /* global browser describe it beforeAll $ */
 
-var assert = require('assert');
+const assert = require('assert');
 
 describe('Basic Video Chat Test', () => {
   beforeAll(() => {
@@ -8,18 +8,21 @@ describe('Basic Video Chat Test', () => {
   });
 
   it('should have the right title', () => {
-    var title = browser.getTitle();
+    const title = browser.getTitle();
     assert.equal(title, 'OpenTok Getting Started');
   });
 
   it('The publisher should load', () => {
-    var publisher = $('div.OT_publisher:not(.OT_loading) .OT_video-element');
+    const publisher = $('div.OT_publisher:not(.OT_loading) .OT_video-element');
     publisher.waitForExist(10000);
   });
 
   it('The subscriber should load if you open a new window', () => {
-    browser.execute('window.open(window.location.href)');
-    var subscriber = $('div.OT_subscriber:not(.OT_loading) .OT_video-element');
+    browser.newWindow('.');
+    const subscriber = $('div.OT_subscriber:not(.OT_loading) .OT_video-element');
     subscriber.waitForExist(10000);
+    browser.switchTab();
+    const subscriber2 = $('div.OT_subscriber:not(.OT_loading) .OT_video-element');
+    subscriber2.waitForExist(5000);
   });
 });
